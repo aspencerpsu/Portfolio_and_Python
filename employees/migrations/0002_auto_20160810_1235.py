7c11c96a92603d70b878e193d8676b6debd5698e
Akeem Spencer <aspencerpsu@gmail.com>
2016-08-21 23:37:48 -0400

    fixed bloggsphere and footer for twitter timeline to come in

---
+++ employees/migrations/0002_auto_20160810_1235.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('employees', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='employee',
+            name='number',
+            field=models.IntegerField(default=11),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='employer',
+            name='number',
+            field=models.IntegerField(default=11),
+            preserve_default=False,
+        ),
+    ]
--- portfolio_revamp/views.py
+++ portfolio_revamp/views.py
@@ -2,12 +2,21 @@ import sys
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import loader
+from django.template.loader import render_to_string
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.template import RequestContext
 from blogs.models import Blogs
+from tweepy import API
+import tweepy
 
 sys.path.append("C:/Python27/Lib/site-packages/django/bin/")
+
+auth = tweepy.OAuthHandler("pX0OETs87mPD0CROfEl5xNSKL", "wOokX8k54HrrWEXGORnarOYzZFiISSVxT90xOJgvX3FcMUX0QB")
+
+auth.set_access_token("457888099-zEIwcezHpVbznLivoMfZ0dzXUgZdN8w3DfcFP1Kl", "cfsPMsazvavdOMQnZUoRUSVzoZHnmuyOv0Kw0UDP1kBSg")
+
+api = tweepy.API(auth)
 
 def sometext(request):
 	return render_to_response('samplehtml.html', {'owner': 'John Marcellus', 'books':'The Kiterunner', 'author':'Khaled Hosseini'})
@@ -15,10 +24,12 @@ def sometext(request):
 
 def index(request):
 	index = open(r'templates/home.html').read()
+	last_5 = api.user_timeline(457888099, None, None, 6, None)
+	second_tweet = last_5[1]
 	# paginator = Paginator([Blogs.objects.first()], 1)
 
 	# page = paginator.page(1)
-	return HttpResponse(index)
+	return render(request, 'home.html', {"id": second_tweet.id_str.encode()} )
 
 def listing(request):
 	contact_list = Contacts.objects.all()
