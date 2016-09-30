3f08286dd754cedef97354f0ece951c9e2630022
Akeem Spencer <aspencerpsu@gmail.com>
2016-09-30 15:44:17 -0400

    templates for site moved within source

--- templates/myfirstview.py
+++
@@ -1,5 +0,0 @@
-# from django.http import HttpResponse
-from django.shortcuts import render_to_response
-
-def sometext(request):
-    return render_to_response('samplehtml.html', {'owner': 'Akeem Warner', 'books': 'The Kiterunner', 'author': 'Khaled Hosseini'})
