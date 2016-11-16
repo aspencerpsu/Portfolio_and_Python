a236201b2a6f4b01e365f3dae4dfc49916d01bb0
Akeem Leighton Foster Spencer <akeem@schwartzenegger.aint.on.no.joke.sh1t>
2016-11-16 09:04:49 -0500

    contact page finished

--- src/portfolio_revamp/views.py
+++ src/portfolio_revamp/views.py
@@ -22,11 +22,16 @@ auth.set_access_token("457888099-zEIwcezHpVbznLivoMfZ0dzXUgZdN8w3DfcFP1Kl", "cfs
 api = tweepy.API(auth)
 
 def main(request):
-	index = open(r'templates/home.html').read()
 	last_5 = api.user_timeline(457888099, None, None, 6, None)
 	second_tweet = last_5[1]
+	
+	context = {
+			"last_5": last_5,
+			"second_tweet": second_tweet,
+			"title": "Spencer Tech Consulting Homepage",
+		}
 
-	return render(request, "main.html", {"last_5": last_5, "second_tweet": second_tweet})
+	return render(request, "main.html", context)
 
 def listing(request):
 	contact_list = Contacts.objects.all()
@@ -41,7 +46,7 @@ def listing(request):
 	except EmptyPage:
 		#If page is out of range (e.g. 9999), deliver last page of results
 		contacts = paginator.page(paginator.num_pages)
-	return render(request, 'list.html', {'contacts': contacts})
+	return render(request, 'list.html')
 
 
 @xframe_options_exempt
@@ -49,12 +54,15 @@ def about(request):
 	"""The following will have some testimonials of what I've done
 	   and also showcase current projects I'm working on"""
 	
-	return render(request, 'about.html')
+	context = {"title": "What SpencerTech Does"}
+
+	return render(request, 'about.html', context)
 
 def contacts(request):
 	""" Just some information
 	    about myself, including my gmail account and phonenumber"""
-	return render(request, 'contact.html')
+	context = {"title": "How To Reach Me"}
+	return render(request, 'contact.html', context)
 
 def page_cannot_load(request):
 
